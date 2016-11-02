5ad879aa671c23201ba0eb2bfd302a49d9234e16
Kasey Stonehill <kaseys111@users.noreply.github.com>
2016-11-02 15:59:48 -0500

    Actor index and profiles complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,3 +1,60 @@
 class MoviesController < ApplicationController
-  
+
+  def index
+    @allmovies = Movie.all
+  end
+
+  def profile
+    @movie = Movie.find(params[:id])
+  end
+
+  # create
+  def new_form
+  end
+
+  def add_movie
+    @movie = Movie.new
+
+    @movie.title = params[:title]
+    @movie.year = params[:year]
+    @movie.duration = params[:duration]
+    @movie.description = params[:description]
+    @movie.director_id = params[:director_id]
+    @movie.image_url = params[:image_url]
+
+    @movie.save
+    redirect_to("http://localhost:3000/movies/" + @movie.id.to_s)
+  end
+
+  # update
+  def edit_form
+    @movie = Movie.find(params[:id])
+  end
+
+  def update_movie
+    @movie = Movie.find(params[:id])
+
+    @movie.title = params[:title]
+    @movie.year = params[:year]
+    @movie.duration = params[:duration]
+    @movie.description = params[:description]
+    @movie.director_id = params[:director_id]
+    @movie.image_url = params[:image_url]
+
+    @movie.save
+
+    @director = Director.find(params[@movie.director_id])
+
+    redirect_to("http://localhost:3000/movies/" + @movie.id.to_s)
+
+  end
+
+  # delete
+  def delete
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+
+    redirect_to("http://localhost:3000/movies")
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,37 @@ Rails.application.routes.draw do
   get("/directors/:id/delete", { :controller => "directors", :action => "delete"})
 
 
-  # actors/movies
-  get("/movies", { :controller => 'movies', :action => 'index'})
-  get("/actors", { :controller => "actors", :action => "index"})
+  # MOVIES
+  # read
+  get("/movies", { :controller => 'movies', :action => "index"})
+  get("/movies/:id", { :controller => "movies", :action => "profile"})
+
+  # create
+  get("/new_movie", { :controller => "movies", :action => "new_form"})
+  get("/create_movie", { :controller => "movies", :action => "add_movie"})
+
+  # update
+  get("/movies/:id/edit", { :controller => "movies", :action => "edit_form"})
+  get("/movies/:id/updated", { :controller => "movies", :action => "update_movie"})
+
+  # delete
+  get("/movies/:id/delete", { :controller => "movies", :action => "delete"})
+
+
+  # ACTORS
+  # read
+  get("/actors", { :controller => 'actors', :action => "index"})
+  get("/actors/:id", { :controller => "actors", :action => "profile"})
+
+  # create
+  get("/new_actor", { :controller => "actors", :action => "new_form"})
+  get("/create_actor", { :controller => "actors", :action => "add_actor"})
+
+  # update
+  get("/actors/:id/edit", { :controller => "actors", :action => "edit_form"})
+  get("/actors/:id/updated", { :controller => "actors", :action => "update_actor"})
+
+  # delete
+  get("/actors/:id/delete", { :controller => "actors", :action => "delete"})
+
 end
