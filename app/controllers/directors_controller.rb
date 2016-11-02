1a1cd83119306990f3d19a657812c0e9ea287550
Kasey Stonehill <kaseys111@users.noreply.github.com>
2016-11-02 15:26:38 -0500

    Director files completed

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -8,19 +8,46 @@ class DirectorsController < ApplicationController
     @director = Director.find(params[:id])
   end
 
+  # create
   def new_form
   end
 
   def add_director
+    @director = Director.new
+
+    @director.dob = params[:dob]
+    @director.name = params[:name]
+    @director.bio = params[:bio]
+    @director.image_url = params[:image_url]
+
+    @director.save
+    redirect_to("http://localhost:3000/directors/" + @director.id.to_s)
   end
 
+  # update
   def edit_form
+    @director = Director.find(params[:id])
   end
 
   def update_director
+    @director = Director.find(params[:id])
+
+    @director.dob = params[:dob]
+    @director.name = params[:name]
+    @director.bio = params[:bio]
+    @director.image_url = params[:image_url]
+
+    @director.save
+    redirect_to("http://localhost:3000/directors/" + @director.id.to_s)
+    
   end
 
+  # delete
   def delete
+    @director = Director.find(params[:id])
+    @director.destroy
+
+    redirect_to("http://localhost:3000/directors")
   end
 
 end
